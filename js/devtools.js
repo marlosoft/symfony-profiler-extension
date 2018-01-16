ef12a038536c5b2f93fa8082a8f3e1b8cfdd40e6
marlosoft <info@marlosoft.net>
2018-01-16 14:10:59 +0800

    initial commit

---
+++ js/devtools.js
@@ -0,0 +1,45 @@
+var profiler;
+chrome.devtools.panels.create(
+    'Symfony Profiler',
+    '/img/symfony01.png',
+    '/pages/panel.html',
+    function (panel) {
+        function setProfilerPanel(p) {
+            profiler = p;
+        }
+
+        panel.onShown.addListener(setProfilerPanel);
+        panel.onHidden.addListener(setProfilerPanel);
+    }
+);
+
+var navigatedURL;
+chrome.devtools.network.onNavigated.addListener(function (url) {
+    navigatedURL = url;
+});
+
+chrome.devtools.network.onRequestFinished.addListener(function (request) {
+    if (navigatedURL !== request.request.url) {
+        return;
+    }
+
+    var responseHeaders = request.response.headers;
+    if (responseHeaders.length < 1) {
+        return;
+    }
+
+    var headerFound = false;
+    var panelURL = '/pages/index.html';
+    for (var x = 0; x < responseHeaders.length; x++) {
+        var header = responseHeaders[x];
+        if (header.name === 'X-Debug-Token-Link') {
+            panelURL = header.value;
+            headerFound = true;
+            break;
+        }
+    }
+
+    profiler.document
+        .getElementById('symfony-profiler')
+        .setAttribute('src', panelURL);
+});
